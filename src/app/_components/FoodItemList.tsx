5fe9c88cc1ae4c15df27909c7142b12a76dd7d0b
sureshzz <sureshthapamagar8914@gmail.com>
2025-01-23 23:52:12 +0545

    foodItemlist

--- src/app/_components/FoodItemList.tsx
+++ src/app/_components/FoodItemList.tsx
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const FoodItemList = () => {
+  useEffect(()=>{
+     loadFoodItems();
+
+  })
+
+  const loadFoodItems = async ()=>{
+    let response = await fetch(
+      "http://localhost:3000/api/restaurants/foods/6754a98331d39c35ace0deed"
+    );
+    response = await response.json();
+    console.log(response);
+
+  }
+
   return (
     <>
       <div>FoodItemList</div>
@@ -11,8 +25,18 @@ const FoodItemList = () => {
             <td>Name</td>
             <td>Price</td>
             <td>Description</td>
+            <td>Operations</td>
           </tr>
         </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Burger</td>
+            <td>22</td>
+            <td>large one</td>
+            <td><button>edit</button><button>Delete</button></td>
+          </tr>
+        </tbody>
       </table>
     </>
   );
